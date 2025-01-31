80eb01f002972fede1fb4015e1fa2e7a89e26257
Paul Aigbokhai Olukayode <paul.olukayode.pro@gmail.com>
2025-01-31 18:41:22 +0100

    feat: add flow to handle ui threat support

--- app/src/main/java/its/dart/com/presentation/viewmodel/OrderViewModel.kt
+++ app/src/main/java/its/dart/com/presentation/viewmodel/OrderViewModel.kt
@@ -1,12 +1,16 @@
 package its.dart.com.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import its.dart.com.domain.repository.remote.model.ProductModel
 import its.dart.com.domain.usecases.OrderUseCases
 import its.dart.com.presentation.viewmodel.state.GenericState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class OrderViewModel  @Inject constructor(private val orderUserCase: OrderUseCases) : ViewModel(){
@@ -14,8 +18,17 @@ class OrderViewModel  @Inject constructor(private val orderUserCase: OrderUseCas
     private val _productState = MutableStateFlow<GenericState<List<ProductModel>>>(GenericState.Loading)
     val productState: StateFlow<GenericState<List<ProductModel>>> = _productState.asStateFlow()
 
-
-
-
+    fun getCustomers() {
+        viewModelScope.launch {
+            _productState.value = GenericState.Loading
+            orderUserCase.fetchProduct()
+                .catch { exception ->
+                    _productState.value = GenericState.Failure(exception)
+                }
+                .collect { result ->
+                    _productState.value = GenericState.Success(result)
+                }
+        }
+    }
 }
 
